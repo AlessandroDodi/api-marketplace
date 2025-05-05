5d95f8e4ae3397eb3214df22519be52b58af957a
AlessandroDodi <dodialessandro3@gmail.com>
2025-05-05 19:28:48 +0200

    Fix production error

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -3,14 +3,16 @@ import CustomAppHeader from "./CustomAppHeader";
 
 const PageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Stack padding={0} maxWidth={1400} alignContent={"center"}>
-      <CustomAppHeader />
-      <Stack
-        sx={{
-          padding: { xs: 1.5, md: 2, lg: 3 },
-        }}
-      >
-        {children}
+    <Stack alignItems={"center"}>
+      <Stack padding={0} maxWidth={1400}>
+        <CustomAppHeader />
+        <Stack
+          sx={{
+            padding: { xs: 1.5, md: 2, lg: 3 },
+          }}
+        >
+          {children}
+        </Stack>
       </Stack>
     </Stack>
   );
